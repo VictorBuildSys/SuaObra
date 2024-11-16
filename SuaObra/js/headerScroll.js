02382d671cd12cc353a280d2eb3a2358d846883e
Drocha <optedev@gmail.com>
2024-11-16 09:55:17 -0300

    ajust

--- SuaObra/js/headerScroll.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("scroll", function() {
-    const header = document.querySelector("header");
-    if (window.scrollY > 0) {
-        header.style.backgroundColor = "rgba(0, 8, 20, 0.9)";
-    } else {
-        header.style.backgroundColor = "rgba(0, 8, 20, 0)";
-    }
-});
