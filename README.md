d64588b857b3329aa92852d0816f4260823fbfa9
Drocha <optedev@gmail.com>
2024-11-22 15:44:33 -0300

    FireBaseHosting

---
+++ README.md
@@ -0,0 +1,36 @@
+# app
+
+## IDE recomendada
+
+```sh
+Visual Code
+```
+
+## To Run:
+
+```sh
+1. `git clone https://github.com/biulddll/puc.git`
+2. `cd puc`
+3. `npm install`
+4. `npm run build`
+5. `npm start`
+6. [http://localhost:3000](http://localhost:3000)
+
+```
+
+## API Teste Mock Vercel
+
+```sh
+https://json-server-vercel-liart.vercel.app/usuario
+```
+
+## Vercel Referencias
+
+```sh
+link
+https://www.youtube.com/watch?v=A--aYRbzKdU
+
+Fork Git Vercel
+https://github.com/biulddll/json-server-vercel
+
+```
---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+// Criando uma instância do Express
+const app = express();
+const path = require('path');
+
+const router = express.Router();
+app.use('/',router);
+app.use(express.static(path.join(__dirname)));
+
+
+
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname,  'index.html'));
+})
+
+router.get('/simone', function (req, res) {
+    res.sendFile(path.join(__dirname,  'simone/index.html'));
+})
+
+
+app.get('/victor', (req, res) => {    
+    res.sendFile(path.join(__dirname,  'victor/index.html'));
+});
+
+
+app.get('/particular', (req, res) => {    
+    res.sendFile(path.join(__dirname,  'particular.html'));
+});
+
+
+app.get('/planos', (req, res) => {    
+    res.sendFile(path.join(__dirname,  'planos.html'));
+});
+
+
+app.get('/register', (req, res) => {    
+    res.sendFile(path.join(__dirname, 'register.html'));
+});
+
+
+app.get('/saibamais', (req, res) => {    
+    res.sendFile(path.join(__dirname,  'saibamais.html'));
+});
+
+
+app.get('/template', (req, res) => {    
+    res.sendFile(path.join(__dirname,  'template.html'));
+});
+
+// Configurando o servidor para escutar na porta 3000
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Servidor está rodando em http://localhost:${port}`);
+});
+
+
